8cbe54ab7379c80677ce691b3385faef101ff789
mahmud-antor <antor.mahmud96@gmail.com>
2019-11-18 17:37:19 +0600

    Move pieces to boxes

--- dev_chess.cpp
+++ dev_chess.cpp
@@ -1,7 +1,7 @@
 #include<SFML/Graphics.hpp>
-#include<bits/stdc++.h>
+//#include<bits/stdc++.h>
 using namespace sf;
-using namespace std;
+//using namespace std;
 
 Sprite fig[33];
 int sze = 56;
@@ -25,14 +25,15 @@ void load_position()
         for(int j=0; j<8; j++)
         {
             int n = board[i][j];
-            if(!n) continue;
+            if(!n)
+                continue;
 
             int x = abs(n)-1;
             int y = n > 0 ? 1:0;
             fig[k].setTextureRect(IntRect(sze*x, sze*y, sze, sze));
-            printf("%d %d %d %d\n", sze*x, sze*y, sze, sze);
+            //printf("%d %d %d %d\n", sze*x, sze*y, sze, sze);
             fig[k].setPosition(sze*j, sze*i);
-            printf("set position = %d %d\n", sze*j, sze*i);
+            //printf("set position = %d %d\n", sze*j, sze*i);
             k++;
         }
     }
@@ -41,7 +42,7 @@ main()
 {
     RenderWindow window(VideoMode(453,454), "My board", Style::Default);
     Texture pieces, board,trial;
-    //trial.loadFromFile("images/figures4.png");
+    // trial.loadFromFile("images/figures4.png");
     board.loadFromFile("images/board0.png");
     pieces.loadFromFile("images/figures.png");
     for(int i=0; i<32; i++)
@@ -57,6 +58,7 @@ main()
     load_position();
     bool isMove = false;
     float dx=0, dy=0;
+    int n=0;
     while(window.isOpen())
     {
         Vector2i pos = Mouse::getPosition(window);
@@ -68,15 +70,20 @@ main()
             {
                 window.close();
             }
+
             if(evnt.type == Event::MouseButtonPressed)
             {
                 if(evnt.key.code == Mouse::Left)
                 {
-                    if(s.getGlobalBounds().contains(pos.x, pos.y))
+                    for(int i=0; i<32; i++)
                     {
-                        isMove = true;
-                        dx = pos.x - s.getPosition().x;
-                        dy = pos.y - s.getPosition().y;
+                        if(fig[i].getGlobalBounds().contains(pos.x, pos.y))
+                        {
+                            isMove = true;
+                            n=i;
+                            dx = pos.x - fig[i].getPosition().x;
+                            dy = pos.y - fig[i].getPosition().y;
+                        }
                     }
                 }
             }
@@ -85,17 +92,23 @@ main()
                 if(evnt.key.code == Mouse::Left)
                 {
                     isMove = false;
+                    Vector2f p = fig[n].getPosition() + Vector2f(sze/2,sze/2);
+                    Vector2f NewPos = Vector2f( sze*int(p.x/sze), sze*int(p.y/sze));
+                    fig[n].setPosition(NewPos);
+                    printf("%f %f %f %f\n", p.x, p.y, NewPos.x,NewPos.y);
                 }
             }
 
         }
+
         if(isMove)
         {
-            s.setPosition(pos.x-dx, pos.y-dy);
+            fig[n].setPosition(pos.x-dx, pos.y-dy);
         }
         window.clear();
         window.draw(sp_brd);
-        for(int i=0; i<32; i++) window.draw(fig[i]);
+        for(int i=0; i<32; i++)
+            window.draw(fig[i]);
         window.display();
     }
 
